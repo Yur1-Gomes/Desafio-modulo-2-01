4227a94f63d1f88392e55af11ad7a2a5677864c7
Yur1-Gomes <yuri2912@hotmail.com>
2022-01-13 18:50:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Yuri";
+let numero = 85;
+let ano = 2022 - numero;
+console.log(ano);
+console.log(ano + numero);
+console.log("Estou mexendo no console.log");
+let quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
